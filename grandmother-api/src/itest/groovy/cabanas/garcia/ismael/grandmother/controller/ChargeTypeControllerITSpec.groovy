d274ae51ea21460bc3a9a30fb38bda8abff597ef
XI317311 <ismael.cabanas@extern.isban.com>
2016-12-12 13:08:45 +0100

    Refactorización
    En este commit se ha refactorizado el tests de integración

--- grandmother-api/src/itest/groovy/cabanas/garcia/ismael/grandmother/controller/ChargeTypeControllerITSpec.groovy
+++ grandmother-api/src/itest/groovy/cabanas/garcia/ismael/grandmother/controller/ChargeTypeControllerITSpec.groovy
@@ -1,40 +1,18 @@
 package cabanas.garcia.ismael.grandmother.controller
 
 import cabanas.garcia.ismael.grandmother.domain.account.ChargeType
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
-import org.springframework.test.context.ContextConfiguration
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by XI317311 on 09/12/2016.
  */
-@ContextConfiguration // not mentioned by docs, but had to include this for Spock to startup the Spring context
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ChargeTypeControllerITSpec extends Specification{
+class ChargeTypeControllerITSpec extends RestIntegrationBaseSpec{
 
-    @Value('${local.server.port}')
-    int port
-
-    @Autowired
-    TestRestTemplate restTemplate = new TestRestTemplate()
-
-    def "should return 201 status code when create a charge type"(){
-        given:
-            ChargeType chargeType = new ChargeType(name: "Agua")
-            RequestEntity<ChargeType> requestEntity = RequestEntity.post(serviceURI("chargeTypes")).body(chargeType)
-        when:
-            ResponseEntity<Void> response = restTemplate.exchange(requestEntity, Void.class)
-        then:
-            response.statusCode == HttpStatus.CREATED
-    }
-
-    def "should return 400 status code when create a charge type without required params"(){
+    @Unroll
+    def "should return #statusCodeExpected status code when create a charge type with name '#chargeTypeName'"(){
         given:
             ChargeType chargeType = new ChargeType(name: chargeTypeName)
             RequestEntity<ChargeType> requestEntity = RequestEntity.post(serviceURI("chargeTypes")).body(chargeType)
@@ -46,9 +24,7 @@ class ChargeTypeControllerITSpec extends Specification{
         chargeTypeName | statusCodeExpected
         ""             | HttpStatus.BAD_REQUEST
         null           | HttpStatus.BAD_REQUEST
+        "Agua"         | HttpStatus.CREATED
     }
 
-    URI serviceURI(String path = "") {
-        new URI("http://localhost:$port/${path}")
-    }
 }
---
+++ grandmother-api/src/itest/groovy/cabanas/garcia/ismael/grandmother/controller/RestIntegrationBaseSpec.groovy
@@ -0,0 +1,26 @@
+package cabanas.garcia.ismael.grandmother.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+/**
+ * Created by XI317311 on 12/12/2016.
+ */
+@ContextConfiguration // not mentioned by docs, but had to include this for Spock to startup the Spring context
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+abstract class RestIntegrationBaseSpec extends Specification{
+
+    @Value('${local.server.port}')
+    int port
+
+    @Autowired
+    TestRestTemplate restTemplate = new TestRestTemplate()
+
+    URI serviceURI(String path = "") {
+        new URI("http://localhost:$port/${path}")
+    }
+}
