baa3e8032fd4a97281a4e36690dc273b27880125
XI317311 <ismael.cabanas@extern.isban.com>
2016-12-12 12:52:20 +0100

    API de creación de tipos de cargo
    En este commit se ha implementado el controlador del API de creación de
    tipos de cargos.

---
+++ grandmother-api/src/main/groovy/cabanas/garcia/ismael/grandmother/controller/ChargeTypeController.groovy
@@ -0,0 +1,33 @@
+package cabanas.garcia.ismael.grandmother.controller
+
+import cabanas.garcia.ismael.grandmother.domain.account.ChargeType
+import cabanas.garcia.ismael.grandmother.service.ChargeTypeService
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+import javax.validation.Valid
+
+/**
+ * Created by XI317311 on 12/12/2016.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/chargeTypes")
+class ChargeTypeController {
+
+    @Autowired
+    ChargeTypeService chargeTypeService
+
+    @RequestMapping(method = RequestMethod.POST)
+    ResponseEntity<Void> create(@Valid @RequestBody ChargeType chargeType){
+        log.debug("Creating... " + chargeType)
+        chargeTypeService.create(chargeType)
+        new ResponseEntity<Void>(HttpStatus.CREATED)
+    }
+}
