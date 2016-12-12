ccd7fe9187cce6bc46df661f144ffd196f583444
XI317311 <ismael.cabanas@extern.isban.com>
2016-12-12 14:26:44 +0100

    Tests de integración del API de creación de personas
    En este commit se han escrito tests de integración para el API de la
    creación de personas.

---
+++ grandmother-api/src/itest/groovy/cabanas/garcia/ismael/grandmother/controller/PersonControllerITSpec.groovy
@@ -0,0 +1,29 @@
+package cabanas.garcia.ismael.grandmother.controller
+
+import cabanas.garcia.ismael.grandmother.domain.account.ChargeType
+import cabanas.garcia.ismael.grandmother.domain.person.Person
+import org.springframework.http.HttpStatus
+import org.springframework.http.RequestEntity
+import org.springframework.http.ResponseEntity
+import spock.lang.Unroll
+
+/**
+ * Created by XI317311 on 12/12/2016.
+ */
+class PersonControllerITSpec extends RestIntegrationBaseSpec {
+    @Unroll
+    def "should return #statusCodeExpected status code when create a person with name '#name'"(){
+        given:
+            Person person = new Person(name: name)
+            RequestEntity<Person> requestEntity = RequestEntity.post(serviceURI("persons")).body(person)
+        when:
+            ResponseEntity<Void> response = restTemplate.exchange(requestEntity, Void.class)
+        then:
+            response.statusCode == statusCodeExpected
+        where:
+        name   | statusCodeExpected
+        ""     | HttpStatus.BAD_REQUEST
+        null   | HttpStatus.BAD_REQUEST
+        "Agua" | HttpStatus.CREATED
+    }
+}
