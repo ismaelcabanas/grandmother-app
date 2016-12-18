c5f8f4f9289b799360f29f04b1a47b5cec46c2d7
XI317311 <ismael.cabanas@extern.isban.com>
2016-12-18 20:41:37 +0100

    Integración de Swagger con SpringBoot

---
+++ grandmother-api/src/main/groovy/cabanas/garcia/ismael/grandmother/configuration/SwaggerConfig.groovy
@@ -0,0 +1,28 @@
+package cabanas.garcia.ismael.grandmother.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+/**
+ * This class configure swagger for publishing API.
+ *
+ * Created by XI317311 on 18/12/2016.
+ */
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
+}
