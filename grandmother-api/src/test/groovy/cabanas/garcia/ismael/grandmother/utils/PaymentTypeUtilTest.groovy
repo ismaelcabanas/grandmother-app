68c4ed0a5d373b9df49ff0d9902ce56e788be8a5
XI317311 <ismael.cabanas@extern.isban.com>
2016-12-20 13:11:06 +0100

    Utilidades de tipos de pago para tests

---
+++ grandmother-api/src/test/groovy/cabanas/garcia/ismael/grandmother/utils/PaymentTypeUtilTest.groovy
@@ -0,0 +1,10 @@
+package cabanas.garcia.ismael.grandmother.utils
+
+import cabanas.garcia.ismael.grandmother.domain.account.PaymentType
+
+/**
+ * Created by XI317311 on 20/12/2016.
+ */
+class PaymentTypeUtilTest {
+    static PaymentType WATER_PAYMENT = PaymentType.builder().name("Agua").build()
+}
