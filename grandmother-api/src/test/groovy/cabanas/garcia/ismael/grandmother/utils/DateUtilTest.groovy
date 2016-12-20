a3c2c6af6d4b6706b098b857951c7131f77c3628
XI317311 <ismael.cabanas@extern.isban.com>
2016-12-20 13:10:47 +0100

    Utilidades de fechas para tests

---
+++ grandmother-api/src/test/groovy/cabanas/garcia/ismael/grandmother/utils/DateUtilTest.groovy
@@ -0,0 +1,11 @@
+package cabanas.garcia.ismael.grandmother.utils
+
+/**
+ * Utility test class for managing dates.
+ *
+ * Created by XI317311 on 20/12/2016.
+ */
+class DateUtilTest {
+    static Date TODAY = new Date()
+    static Date YESTERDAY = new Date().previous()
+}
