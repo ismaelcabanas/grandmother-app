49211d1e8bc7c1ce4308723e9faf81ba6ae209d0
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-04-27 09:29:15 +0200

    Adding funcionality to stub

--- grandmother-api/src/test/groovy/cabanas/garcia/ismael/grandmother/stubs/service/account/AccountBalanceServiceDefaultStub.groovy
+++ grandmother-api/src/test/groovy/cabanas/garcia/ismael/grandmother/stubs/service/account/AccountBalanceServiceDefaultStub.groovy
@@ -4,11 +4,19 @@ import cabanas.garcia.ismael.grandmother.domain.account.Account
 import cabanas.garcia.ismael.grandmother.service.AccountBalanceService
 
 class AccountBalanceServiceDefaultStub implements AccountBalanceService{
+    Account account
+
+    AccountBalanceServiceDefaultStub() {
+    }
+
     AccountBalanceServiceDefaultStub(Account account) {
+        this.account = account
     }
 
     @Override
     BigDecimal balance(long accountId, int year, int month) {
-        return null
+        if(account != null && account.id.equals(accountId))
+            return account.balance
+        return BigDecimal.ZERO
     }
 }
