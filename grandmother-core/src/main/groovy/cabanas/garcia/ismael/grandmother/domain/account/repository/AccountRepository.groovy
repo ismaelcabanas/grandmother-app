1605d13645faa62a1c02ff04ddc646b564ba5206
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-04-27 11:25:27 +0200

    New method for getting account balance to given year and month

--- grandmother-core/src/main/groovy/cabanas/garcia/ismael/grandmother/domain/account/repository/AccountRepository.groovy
+++ grandmother-core/src/main/groovy/cabanas/garcia/ismael/grandmother/domain/account/repository/AccountRepository.groovy
@@ -3,9 +3,7 @@ package cabanas.garcia.ismael.grandmother.domain.account.repository
 import cabanas.garcia.ismael.grandmother.domain.account.Account
 import org.springframework.data.repository.CrudRepository
 
-/**
- * Created by XI317311 on 09/12/2016.
- */
 interface AccountRepository extends CrudRepository<Account, Long>{
 
+    BigDecimal balance(long accountId, int year, int month)
 }
